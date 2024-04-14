3b82729282ba29981c3e9b515d53fedf354d8b1d
Maksim Vorobyov <m.vorobyov@owlcat.games>
2024-04-14 06:22:46 +0300

    fdgdfgdf

--- frontend/src/components/system/System.jsx
+++ frontend/src/components/system/System.jsx
@@ -7,16 +7,16 @@ export default function System() {
 
   const [pages, setPages] = useState(1)
 
-  const {data} =  useQuery({
+  const { data } = useQuery({
     queryKey: ["data"],
-    queryFn: async() => {
-        const res = await axios.get("https://run.mocky.io/v3/1e0d344b-8888-446a-9e4c-99cce98e917e")
-        return res.data
+    queryFn: async () => {
+      const res = await axios.get("http://127.0.0.1:8000")
+      return res.data
     },
     refetchInterval: 1000
   })
 
-  
+
 
   const changePage = (e, page) => {
     setPages(page)
@@ -29,53 +29,53 @@ export default function System() {
       <ul className="system__result-menu">
         {
           data
-          ?.filter(item => item.id === pages)
-          ?.map(pref => {
-            return(
-             <>
-                <li>
-                  <h4>source_ip:</h4>
-                  <p>{pref?.source_ip}</p>
-                </li>
-                <li>
-                  <h4>destination_ip:</h4>
-                  <p>{pref?.destination_ip}</p>
-                </li>
-                <li>
-                  <h4>protocol:</h4>
-                  <p>{pref?.protocol}</p>
-                </li>
-                <li>
-                  <h4>source_port:</h4>
-                  <p>{pref?.source_port}</p>
-                </li>
-                <li>
-                  <h4>destination_port:</h4>
-                  <p>{pref?.destination_port}</p>
-                </li>
-                <li>
-                  <h4>packet_length:</h4>
-                  <p>{pref?.packet_length}</p>
-                </li>
-                <li>
-                  <h4>flags:</h4>
-                  <p>{pref?.flags}</p>
-                </li>
-                <li>
-                  <h4>threat:</h4>
-                  <p>{pref?.threat}</p>
-                </li>
-                <li>
-                  <h4>is_danger:</h4>
-                  <p style={{color: pref?.is_danger && 'red'}}>{pref?.is_danger}</p>
-                </li>
-             </>
-            )
-          })
+            ?.filter(item => item.id === pages)
+            ?.map(pref => {
+              return (
+                <>
+                  <li>
+                    <h4>source_ip:</h4>
+                    <p>{pref?.source_ip}</p>
+                  </li>
+                  <li>
+                    <h4>destination_ip:</h4>
+                    <p>{pref?.destination_ip}</p>
+                  </li>
+                  <li>
+                    <h4>protocol:</h4>
+                    <p>{pref?.protocol}</p>
+                  </li>
+                  <li>
+                    <h4>source_port:</h4>
+                    <p>{pref?.source_port}</p>
+                  </li>
+                  <li>
+                    <h4>destination_port:</h4>
+                    <p>{pref?.destination_port}</p>
+                  </li>
+                  <li>
+                    <h4>packet_length:</h4>
+                    <p>{pref?.packet_length}</p>
+                  </li>
+                  <li>
+                    <h4>flags:</h4>
+                    <p>{pref?.flags}</p>
+                  </li>
+                  <li>
+                    <h4>threat:</h4>
+                    <p>{pref?.threat}</p>
+                  </li>
+                  <li>
+                    <h4>is_danger:</h4>
+                    <p style={{ color: pref?.is_danger && 'red' }}>{pref?.is_danger}</p>
+                  </li>
+                </>
+              )
+            })
         }
       </ul>
       <div className="system__controllers">
-      <Pagination page={pages} onChange={changePage} count={data?.length} variant="outlined" shape="rounded" />
+        <Pagination page={pages} onChange={changePage} count={data?.length} variant="outlined" shape="rounded" />
       </div>
     </div>
   )
